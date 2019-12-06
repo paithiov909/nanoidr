d5a251693ea2bb9ce21551d38f988647d99aaa50
paithiov909 <am11.44@gmail.com>
2019-12-06 12:24:52 +0900

    v0.1.0-2

--- R/utils.R
+++ R/utils.R
@@ -1,33 +1,35 @@
-#' Pseudo window.crypto.getRandomValues() function
+#' Pseudo crypto.getRandomValues Web API
 #'
-#' The original ai/nanoid generates random strings
-#' with window.crypto.getRandomValues() on client side environment,
-#' however, the V8 environment has no crypto object.
-#' Because of this, this package replaces that function with
-#' an original random value generator.
+#' The original NanoID library generates random strings
+#' with window.crypto.getRandomValues on client side environment,
+#' however, the V8 environment provides no crypto object.
+#' Because of this, {nanoidr} package replaces that function with
+#' a simple wrapper of openssl::rand_bytes().
 #'
-#' @param seed string.
 #' @param size integer (size of output).
-#' @return list of integers.
+#' @param seed only when fix.seed is true, will be used for set.seed(). Or not, will be used just for dqrng::dqset.seed().
+#' @param fix.seed default is false.
+#' @return list of buffer.
 #'
 #' @seealso \url{https://developer.mozilla.org/ja/docs/Web/API/Window/crypto}
 #'
 #' @import dqrng
 #' @import purrr
-#' @importFrom stringr str_split
 #' @importFrom stringi stri_rand_strings
+#' @importFrom openssl rand_bytes
 #' @export
-getRandomValues <- function(seed = NULL, size = 64L) {
-    if (typeof(seed) == "character") {
-        range <- length(stringr::str_split(seed, "", simplify = TRUE))
-        seed <- purrr::reduce(dqrng::dqrunif(range, min = -128L, max = 128L), ~ sum(floor(.)), init = 0)
+getRandomValues <- function(size = 64L, seed = 1234L, fix.seed = FALSE) {
+    if (fix.seed) {
+        set.seed(seed)
     } else {
-        seed <- purrr::reduce(dqrng::dqrunif(size, min = -128L, max = 128L), ~ sum(floor(.)), init = 0)
+        dqrng::dqset.seed(seed)
+        rnd <- purrr::reduce(dqrng::dqrunif(size, min = -128L, max = 128L), ~ sum(floor(.)), init = 0)
+        set.seed(rnd)
     }
-    dqrng::dqset.seed(seed)
-    bytes <- purrr::map(1:size, ~ abs(floor(dqrng::dqrunif(1, min = -128L, max = 128L))))
-    return(bytes)
+    bytes <- openssl::rand_bytes(size)
+    return(as.integer(bytes))
 }
+
 
 #' An example of original random bytes generator for using in fomrat()
 #'
@@ -38,4 +40,7 @@ getRandomValues <- function(seed = NULL, size = 64L) {
 randombytes <- function(size) {
     return(sample(1:64, size, replace = TRUE))
 }
+
+
+
 
--- inst/index.js
+++ inst/index.js
@@ -101,3 +101,4 @@ const nanoidr = {
 })(global);
 
 
+
